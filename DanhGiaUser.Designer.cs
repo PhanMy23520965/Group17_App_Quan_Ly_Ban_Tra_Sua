12b04dbb6c5c24259e2e62288fdb0b4d43cc2eb0
PhanMy23520965 <23520965@gm.uit.edu.vn>
2025-05-18 09:40:48 +0700

    Giao diện đánh giá

---
+++ DanhGiaUser.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TraSuaApp.View
+{
+    partial class DanhGiaUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            panel1 = new Panel();
+            pnlDanhGia = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            pictureBox1 = new PictureBox();
+            guna2GradientButton2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            label1 = new Label();
+            rtbBinhLuan = new RichTextBox();
+            guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            panel1.SuspendLayout();
+            pnlDanhGia.SuspendLayout();
+            guna2Panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(pnlDanhGia);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1412, 876);
+            panel1.TabIndex = 5;
+            // 
+            // pnlDanhGia
+            // 
+            pnlDanhGia.BorderColor = Color.Silver;
+            pnlDanhGia.BorderRadius = 20;
+            pnlDanhGia.BorderThickness = 5;
+            pnlDanhGia.Controls.Add(guna2Panel3);
+            pnlDanhGia.Controls.Add(guna2GradientButton2);
+            pnlDanhGia.Controls.Add(label1);
+            pnlDanhGia.Controls.Add(rtbBinhLuan);
+            pnlDanhGia.Controls.Add(guna2PictureBox3);
+            pnlDanhGia.CustomizableEdges = customizableEdges15;
+            pnlDanhGia.FillColor = Color.White;
+            pnlDanhGia.Location = new Point(192, 91);
+            pnlDanhGia.Name = "pnlDanhGia";
+            pnlDanhGia.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            pnlDanhGia.Size = new Size(984, 753);
+            pnlDanhGia.TabIndex = 0;
+            // 
+            // guna2Panel3
+            // 
+            guna2Panel3.BorderRadius = 20;
+            guna2Panel3.BorderThickness = 3;
+            guna2Panel3.Controls.Add(pictureBox1);
+            customizableEdges9.BottomLeft = false;
+            customizableEdges9.BottomRight = false;
+            guna2Panel3.CustomizableEdges = customizableEdges9;
+            guna2Panel3.Dock = DockStyle.Top;
+            guna2Panel3.FillColor = Color.FromArgb(246, 153, 92);
+            guna2Panel3.Location = new Point(0, 0);
+            guna2Panel3.Name = "guna2Panel3";
+            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel3.Size = new Size(984, 185);
+            guna2Panel3.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.thankyou1;
+            pictureBox1.Location = new Point(155, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(679, 182);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2GradientButton2
+            // 
+            guna2GradientButton2.BorderRadius = 20;
+            guna2GradientButton2.CustomizableEdges = customizableEdges11;
+            guna2GradientButton2.DisabledState.BorderColor = Color.DarkGray;
+            guna2GradientButton2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2GradientButton2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2GradientButton2.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            guna2GradientButton2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2GradientButton2.FillColor2 = Color.FromArgb(255, 128, 0);
+            guna2GradientButton2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2GradientButton2.ForeColor = Color.White;
+            guna2GradientButton2.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            guna2GradientButton2.Location = new Point(272, 643);
+            guna2GradientButton2.Name = "guna2GradientButton2";
+            guna2GradientButton2.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2GradientButton2.Size = new Size(237, 68);
+            guna2GradientButton2.TabIndex = 20;
+            guna2GradientButton2.Text = "GỬI NGAY";
+            guna2GradientButton2.Click += guna2GradientButton2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(80, 316);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 28);
+            label1.TabIndex = 19;
+            label1.Text = "Nhận xét";
+            // 
+            // rtbBinhLuan
+            // 
+            rtbBinhLuan.BackColor = Color.WhiteSmoke;
+            rtbBinhLuan.Location = new Point(80, 347);
+            rtbBinhLuan.Name = "rtbBinhLuan";
+            rtbBinhLuan.Size = new Size(626, 251);
+            rtbBinhLuan.TabIndex = 18;
+            rtbBinhLuan.Text = "";
+            // 
+            // guna2PictureBox3
+            // 
+            guna2PictureBox3.CustomizableEdges = customizableEdges13;
+            guna2PictureBox3.Image = Properties.Resources.like;
+            guna2PictureBox3.ImageRotate = 0F;
+            guna2PictureBox3.Location = new Point(750, 509);
+            guna2PictureBox3.Name = "guna2PictureBox3";
+            guna2PictureBox3.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2PictureBox3.Size = new Size(231, 222);
+            guna2PictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox3.TabIndex = 12;
+            guna2PictureBox3.TabStop = false;
+            // 
+            // DanhGia
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.nền;
+            ClientSize = new Size(1412, 876);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DanhGia";
+            Text = "DanhGia";
+            panel1.ResumeLayout(false);
+            pnlDanhGia.ResumeLayout(false);
+            pnlDanhGia.PerformLayout();
+            guna2Panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Guna.UI2.WinForms.Guna2Panel pnlDanhGia;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton2;
+        private Label label1;
+        private RichTextBox rtbBinhLuan;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private PictureBox pictureBox1;
+    }
+}
