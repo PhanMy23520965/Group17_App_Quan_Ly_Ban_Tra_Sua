ca5d41cc75817b2634bbf0f7cecac080b126ba23
PhanMy23520965 <23520965@gm.uit.edu.vn>
2025-05-18 09:39:53 +0700

    Chỉnh sửa giao diện Chi Tiết Sản Phẩm

---
+++ LienHe.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace TraSuaApp.View
+{
+    partial class LienHe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            pictureBox6 = new PictureBox();
+            linkLabel4 = new LinkLabel();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            lblDiaChi = new Label();
+            llblShopeefood = new LinkLabel();
+            llblInstagram = new LinkLabel();
+            llblFacebook = new LinkLabel();
+            lblSDT = new Label();
+            label9 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2ShadowPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            guna2GradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2ShadowPanel1
+            // 
+            guna2ShadowPanel1.BackColor = Color.Transparent;
+            guna2ShadowPanel1.Controls.Add(pictureBox6);
+            guna2ShadowPanel1.Controls.Add(linkLabel4);
+            guna2ShadowPanel1.Controls.Add(guna2GradientPanel1);
+            guna2ShadowPanel1.Controls.Add(guna2PictureBox3);
+            guna2ShadowPanel1.FillColor = Color.WhiteSmoke;
+            guna2ShadowPanel1.Location = new Point(93, 60);
+            guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            guna2ShadowPanel1.ShadowColor = Color.Black;
+            guna2ShadowPanel1.ShadowShift = 20;
+            guna2ShadowPanel1.Size = new Size(1077, 739);
+            guna2ShadowPanel1.TabIndex = 5;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.tieude;
+            pictureBox6.Location = new Point(138, 44);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(775, 91);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 23;
+            pictureBox6.TabStop = false;
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            linkLabel4.LinkColor = Color.LightSlateGray;
+            linkLabel4.Location = new Point(108, 464);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(590, 30);
+            linkLabel4.TabIndex = 22;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "Tìm đường đến quán tại đây! Cảm ơn bạn đã ghé thăm 💕";
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BorderColor = Color.Silver;
+            guna2GradientPanel1.BorderRadius = 20;
+            guna2GradientPanel1.BorderThickness = 2;
+            guna2GradientPanel1.Controls.Add(pictureBox5);
+            guna2GradientPanel1.Controls.Add(pictureBox4);
+            guna2GradientPanel1.Controls.Add(pictureBox3);
+            guna2GradientPanel1.Controls.Add(pictureBox2);
+            guna2GradientPanel1.Controls.Add(pictureBox1);
+            guna2GradientPanel1.Controls.Add(lblDiaChi);
+            guna2GradientPanel1.Controls.Add(llblShopeefood);
+            guna2GradientPanel1.Controls.Add(llblInstagram);
+            guna2GradientPanel1.Controls.Add(llblFacebook);
+            guna2GradientPanel1.Controls.Add(lblSDT);
+            guna2GradientPanel1.Controls.Add(label9);
+            guna2GradientPanel1.Controls.Add(label4);
+            guna2GradientPanel1.Controls.Add(label3);
+            guna2GradientPanel1.Controls.Add(label2);
+            guna2GradientPanel1.Controls.Add(label1);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges1;
+            guna2GradientPanel1.FillColor = Color.FromArgb(255, 228, 199);
+            guna2GradientPanel1.Location = new Point(108, 141);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2GradientPanel1.Size = new Size(878, 303);
+            guna2GradientPanel1.TabIndex = 21;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.thanhlong;
+            pictureBox5.Location = new Point(43, 235);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(56, 44);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 34;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.thanhlong;
+            pictureBox4.Location = new Point(43, 180);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(56, 44);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 33;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.thanhlong;
+            pictureBox3.Location = new Point(43, 80);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(56, 44);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 32;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.thanhlong;
+            pictureBox2.Location = new Point(43, 130);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(56, 44);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 31;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.thanhlong;
+            pictureBox1.Location = new Point(43, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(56, 44);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 30;
+            pictureBox1.TabStop = false;
+            // 
+            // lblDiaChi
+            // 
+            lblDiaChi.AutoSize = true;
+            lblDiaChi.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            lblDiaChi.ForeColor = Color.Chocolate;
+            lblDiaChi.Location = new Point(347, 249);
+            lblDiaChi.Name = "lblDiaChi";
+            lblDiaChi.Size = new Size(492, 30);
+            lblDiaChi.TabIndex = 29;
+            lblDiaChi.Text = "Hàn Thuyên, khu phố 6 P, Thủ Đức, Hồ Chí Minh";
+            // 
+            // llblShopeefood
+            // 
+            llblShopeefood.AutoSize = true;
+            llblShopeefood.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            llblShopeefood.ForeColor = Color.Chocolate;
+            llblShopeefood.LinkBehavior = LinkBehavior.HoverUnderline;
+            llblShopeefood.LinkColor = Color.FromArgb(1, 6, 153);
+            llblShopeefood.Location = new Point(347, 194);
+            llblShopeefood.Name = "llblShopeefood";
+            llblShopeefood.Size = new Size(176, 30);
+            llblShopeefood.TabIndex = 28;
+            llblShopeefood.TabStop = true;
+            llblShopeefood.Text = "ThanhLongShop";
+            // 
+            // llblInstagram
+            // 
+            llblInstagram.AutoSize = true;
+            llblInstagram.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            llblInstagram.ForeColor = Color.Chocolate;
+            llblInstagram.LinkBehavior = LinkBehavior.HoverUnderline;
+            llblInstagram.LinkColor = Color.FromArgb(1, 6, 153);
+            llblInstagram.Location = new Point(347, 144);
+            llblInstagram.Name = "llblInstagram";
+            llblInstagram.Size = new Size(190, 30);
+            llblInstagram.TabIndex = 27;
+            llblInstagram.TabStop = true;
+            llblInstagram.Text = "ThanhLongMT123";
+            // 
+            // llblFacebook
+            // 
+            llblFacebook.AutoSize = true;
+            llblFacebook.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            llblFacebook.ForeColor = Color.Chocolate;
+            llblFacebook.LinkBehavior = LinkBehavior.HoverUnderline;
+            llblFacebook.LinkColor = Color.FromArgb(1, 6, 153);
+            llblFacebook.Location = new Point(347, 94);
+            llblFacebook.Name = "llblFacebook";
+            llblFacebook.Size = new Size(202, 30);
+            llblFacebook.TabIndex = 26;
+            llblFacebook.TabStop = true;
+            llblFacebook.Text = "ThanhLongMilkTea";
+            // 
+            // lblSDT
+            // 
+            lblSDT.AutoSize = true;
+            lblSDT.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            lblSDT.ForeColor = Color.Chocolate;
+            lblSDT.Location = new Point(347, 41);
+            lblSDT.Name = "lblSDT";
+            lblSDT.Size = new Size(133, 30);
+            lblSDT.TabIndex = 25;
+            lblSDT.Text = "0909090909";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label9.Location = new Point(96, 247);
+            label9.Name = "label9";
+            label9.Size = new Size(94, 32);
+            label9.TabIndex = 24;
+            label9.Text = "Địa chỉ:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label4.Location = new Point(96, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(203, 32);
+            label4.TabIndex = 23;
+            label4.Text = "Link Shopeefood:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label3.Location = new Point(96, 142);
+            label3.Name = "label3";
+            label3.Size = new Size(182, 32);
+            label3.TabIndex = 22;
+            label3.Text = "Link Instagram:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label2.Location = new Point(96, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 32);
+            label2.TabIndex = 21;
+            label2.Text = "Link Facebook:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label1.Location = new Point(96, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 32);
+            label1.TabIndex = 20;
+            label1.Text = "Số điện thoại liên hệ:";
+            // 
+            // guna2PictureBox3
+            // 
+            guna2PictureBox3.CustomizableEdges = customizableEdges3;
+            guna2PictureBox3.Image = Properties.Resources.liketrasua1;
+            guna2PictureBox3.ImageRotate = 0F;
+            guna2PictureBox3.Location = new Point(653, 464);
+            guna2PictureBox3.Name = "guna2PictureBox3";
+            guna2PictureBox3.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2PictureBox3.Size = new Size(409, 248);
+            guna2PictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox3.TabIndex = 20;
+            guna2PictureBox3.TabStop = false;
+            // 
+            // LienHe
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.nền;
+            ClientSize = new Size(1412, 876);
+            Controls.Add(guna2ShadowPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "LienHe";
+            Text = "LienHe";
+            Load += LienHe_Load;
+            guna2ShadowPanel1.ResumeLayout(false);
+            guna2ShadowPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            guna2GradientPanel1.ResumeLayout(false);
+            guna2GradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label lblDiaChi;
+        private LinkLabel llblShopeefood;
+        private LinkLabel llblInstagram;
+        private LinkLabel llblFacebook;
+        private Label lblSDT;
+        private Label label9;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private LinkLabel linkLabel4;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private PictureBox pictureBox6;
+    }
+}
